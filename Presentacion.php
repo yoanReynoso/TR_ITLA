170095b028e0fb59472f371f49197c7ecf47ca9d
yoan reynoso <yoan-reynoso@hotmail.com>
2019-09-20 01:39:57 -0400

    esta es la primera tarea

---
+++ Presentacion.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+   
+   <?php
+    
+     echo "
+     <br>
+   
+     <h3>Mi nombre es:</h3>
+
+     <h3>yoan reynoso</h3>
+
+     <h3> Mi esperiesa en el aria: <h3>
+
+     <p>
+         <h3> 
+          Soy estudiante de la uasd no tengo mucha pero me encata la <br>
+          programacion megustaria tener mas tiempo para programar pero <br>
+          hasta que consiga untrabajo para de programador 
+
+         </h3>   
+
+      </p>
+
+         <h3>Mis hobbies:</h3>
+
+      <p> 
+
+        <h3>
+           bueno que puedo de decir me gusta el anime y ver televicion y <br>
+           leer un libro de ves en cuando 
+        </h3>
+     
+        </p>
+
+     ";
+   
+     ?>
+
+
+
+
+</body>
+</html>
