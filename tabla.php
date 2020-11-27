19a2042435ca26406254af24f170e87bbb21b152
yoan reynoso <yoan-reynoso@hotmail.com>
2020-11-26 23:30:29 -0400

    esta listo

--- tabla.php
+++ tabla.php
@@ -10,7 +10,7 @@
 <body>
 
 
-       <section id= ""> <h1>Tabla Periodica de los Helementos </h1></section>
+       <section id= "titulo"> <h1>Tabla Periodica de los Helementos </h1></section>
      
 <?php
 
